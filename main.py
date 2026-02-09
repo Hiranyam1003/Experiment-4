92a489450ddd9d66d18fd0d36b17b3e506f4b4e3
Hiranyam Gupta <hiranyamgupta@gmail.com>
2026-02-09 08:54:30 +0530

    Correction of functions into main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #Calculator Program 
 
-import add 
-import sub 
-import mul 
+import addition as add
+import subtraction as sub
+import multiplication as mul 
 print(f"2 + 3 = {add(2, 3)}") 
 print(f"10 - 4 = {sub(10, 4)}")
 print(f"5 * 6 = {mul(5, 6)}") 
