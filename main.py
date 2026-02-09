66dc521a5879caf004c425a8547474c48a8cd3f4
Hiranyam Gupta <hiranyamgupta@gmail.com>
2026-02-09 08:56:40 +0530

    Print statements corrected

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 import addition as add
 import subtraction as sub
 import multiplication as mul 
-print(f"2 + 3 = {add(2, 3)}") 
-print(f"10 - 4 = {sub(10, 4)}")
-print(f"5 * 6 = {mul(5, 6)}") 
+print(f"2 + 3 = {add.add(2, 3)}")
+print(f"10 - 4 = {sub.sub(10, 4)}")
+print(f"5 * 6 = {mul.mul(5, 6)}") 
